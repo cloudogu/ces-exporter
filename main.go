ec8dbdafd018481aeb94b7d47c0fa5c63f3d5580
kahoona77 <benjamin.ernst@cloudogu.com>
2025-03-14 13:33:20 +0100

    Apply suggestions from code review
    Co-authored-by: sklein94 <30648955+sklein94@users.noreply.github.com>

--- main.go
+++ main.go
@@ -73,8 +73,8 @@ func createServer(config core.Configuration) http.Handler {
 
 	rootHandler.HandleFunc("GET /configuration", authMiddleware(configuration.GetConfig))
 
-	rootHandler.HandleFunc("GET /export/{doguName}", authMiddleware(export.GetExportDogu))
-	rootHandler.HandleFunc("POST /export/{doguName}", authMiddleware(export.SetExportDogu))
+	rootHandler.HandleFunc("GET /export/dogu/{doguName}", authMiddleware(export.GetExportDogu))
+	rootHandler.HandleFunc("POST /export/dogu/{doguName}", authMiddleware(export.SetExportDogu))
 	rootHandler.HandleFunc("GET /export/mode", authMiddleware(export.GetExportMode))
 
 	rootHandler.HandleFunc("GET /maintenance/mode", authMiddleware(maintenance.GetMaintenanceMode))
