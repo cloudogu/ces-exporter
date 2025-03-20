23a8bb2ec3eee13e0cc8a4e5422c0c5909a6576d
Simon Klein <simon.klein@cloudogu.com>
2025-03-20 15:18:58 +0100

    #6 Revert debug change

--- core/auth_middleware.go
+++ core/auth_middleware.go
@@ -9,12 +9,12 @@ const apiKeyHeaderName = "X-CES-EXPORTER-API-KEY"
 func NewAuthMiddleware(config Configuration) func(next http.HandlerFunc) http.HandlerFunc {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			//apiKey := r.Header.Get(apiKeyHeaderName)
-			//
-			//if apiKey != config.ApiKey {
-			//	Unauthorized(w, http.StatusText(http.StatusUnauthorized))
-			//	return
-			//}
+			apiKey := r.Header.Get(apiKeyHeaderName)
+
+			if apiKey != config.ApiKey {
+				Unauthorized(w, http.StatusText(http.StatusUnauthorized))
+				return
+			}
 
 			w.Header().Add("Access-Control-Allow-Origin", "*")
 
