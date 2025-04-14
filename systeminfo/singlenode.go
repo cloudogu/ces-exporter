616f4da9cfc0f1f245c64c068bd1ca52f02c33ab
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-04-14 14:48:01 +0200

    [#17] Implement getDogus with their name und version

--- systeminfo/singlenode.go
+++ systeminfo/singlenode.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/cloudogu/ces-exporter/core"
+	cesLibCore "github.com/cloudogu/cesapp-lib/core"
 )
 
 var (
@@ -12,15 +13,21 @@ var (
 
 type getGlobalConfigFunc func(ignoreKeys []string) (core.GlobalConfig, error)
 
+type doguGetter interface {
+	GetAllDogus() ([]string, error)
+	GetDoguSpec(dogu string) (cesLibCore.Dogu, error)
+}
+
 type SingleNodeSystemInfoProvider struct {
 	getGlobalConfig getGlobalConfigFunc
+	doguGetter
 }
 
 func NewSingleNodeSystemInfoProvider() *SingleNodeSystemInfoProvider {
 	return &SingleNodeSystemInfoProvider{}
 }
 
-func (sp *SingleNodeSystemInfoProvider) getFqdn() (string, error) {
+func (sp SingleNodeSystemInfoProvider) getFqdn() (string, error) {
 	globalCfg, err := sp.getGlobalConfig(nil)
 	if err != nil {
 		return "", fmt.Errorf("error getting global config: %w", err)
@@ -33,4 +40,28 @@ func (sp *SingleNodeSystemInfoProvider) getFqdn() (string, error) {
 	}
 
 	return "", errFqdnGlobalConfigKeyNotFound
+}
+
+func (sp SingleNodeSystemInfoProvider) getDogus() ([]dogu, error) {
+	doguList, err := sp.GetAllDogus()
+	if err != nil {
+		return nil, fmt.Errorf("error getting currently installed dogus: %w", err)
+	}
+
+	doguSystemInfoList := make([]dogu, 0, len(doguList))
+
+	for _, d := range doguList {
+		doguSpec, lErr := sp.GetDoguSpec(d)
+		if lErr != nil {
+			return nil, fmt.Errorf("error getting doguSpec for dogu %s: %w", d, lErr)
+		}
+
+		doguSystemInfoList = append(doguSystemInfoList, dogu{
+			Name:    doguSpec.Name,
+			Version: doguSpec.Version,
+			Volume:  volume{SizeInBytes: 0},
+		})
+	}
+
+	return doguSystemInfoList, nil
 }
--- systeminfo/singlenode_test.go
+++ systeminfo/singlenode_test.go
@@ -2,6 +2,7 @@ package systeminfo
 
 import (
 	"github.com/cloudogu/ces-exporter/core"
+	cesLibCore "github.com/cloudogu/cesapp-lib/core"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -57,4 +58,92 @@ func TestSingleNodeSystemInfoProvider_getFqdn(t *testing.T) {
 		assert.ErrorIs(t, err, errFqdnGlobalConfigKeyNotFound)
 	})
 
+}
+
+func TestSingleNodeSystemInfoProvider_getDogus(t *testing.T) {
+	t.Run("return current dogus", func(t *testing.T) {
+		adminDoguSpec := cesLibCore.Dogu{
+			Name:    "premium/admin",
+			Version: "2.13.2-1",
+		}
+
+		usermgtDoguSpec := cesLibCore.Dogu{
+			Name:    "usermgt/admin",
+			Version: "1.20.0-4",
+		}
+
+		confluenceDoguSpec := cesLibCore.Dogu{
+			Name:    "premium/confluence",
+			Version: "8.5.19-1",
+		}
+
+		expDoguMap := map[string]cesLibCore.Dogu{
+			adminDoguSpec.Name:      adminDoguSpec,
+			usermgtDoguSpec.Name:    usermgtDoguSpec,
+			confluenceDoguSpec.Name: confluenceDoguSpec,
+		}
+
+		doguGetterMock := newMockDoguGetter(t)
+
+		doguGetterMock.EXPECT().GetAllDogus().Return([]string{"admin", "usermgt", "confluence"}, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("admin").Return(adminDoguSpec, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("usermgt").Return(usermgtDoguSpec, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("confluence").Return(confluenceDoguSpec, nil)
+
+		sp := SingleNodeSystemInfoProvider{
+			getGlobalConfig: nil,
+			doguGetter:      doguGetterMock,
+		}
+
+		dogus, err := sp.getDogus()
+		assert.NoError(t, err)
+		assert.Equal(t, len(expDoguMap), len(dogus))
+
+		for _, d := range dogus {
+			expDoguSpec, ok := expDoguMap[d.Name]
+			assert.True(t, ok)
+			assert.Equal(t, expDoguSpec.Version, d.Version)
+		}
+	})
+
+	t.Run("error getting all dogus currently installed", func(t *testing.T) {
+		doguGetterMock := newMockDoguGetter(t)
+
+		doguGetterMock.EXPECT().GetAllDogus().Return(nil, assert.AnError)
+
+		sp := SingleNodeSystemInfoProvider{
+			getGlobalConfig: nil,
+			doguGetter:      doguGetterMock,
+		}
+
+		_, err := sp.getDogus()
+		assert.ErrorIs(t, err, assert.AnError)
+	})
+
+	t.Run("error receiving dogu spec", func(t *testing.T) {
+		adminDoguSpec := cesLibCore.Dogu{
+			Name:    "premium/admin",
+			Version: "2.13.2-1",
+		}
+
+		usermgtDoguSpec := cesLibCore.Dogu{
+			Name:    "usermgt/admin",
+			Version: "1.20.0-4",
+		}
+
+		doguGetterMock := newMockDoguGetter(t)
+
+		doguGetterMock.EXPECT().GetAllDogus().Return([]string{"admin", "usermgt", "confluence"}, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("admin").Return(adminDoguSpec, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("usermgt").Return(usermgtDoguSpec, nil)
+		doguGetterMock.EXPECT().GetDoguSpec("confluence").Return(cesLibCore.Dogu{}, assert.AnError)
+
+		sp := SingleNodeSystemInfoProvider{
+			getGlobalConfig: nil,
+			doguGetter:      doguGetterMock,
+		}
+
+		_, err := sp.getDogus()
+		assert.ErrorIs(t, err, assert.AnError)
+	})
 }
